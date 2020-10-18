6c53e70ad13a4856a2a321dff8cf6f244e5db78c
Kamila <wiewiora145@gmail.com>
2020-10-18 16:33:56 +0200

    [2] Tiles plugin

--- src/main/java/skamila/kapj/configuration/MyConfiguration.java
+++ src/main/java/skamila/kapj/configuration/MyConfiguration.java
@@ -2,22 +2,32 @@ package skamila.kapj.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("skamila.kapj")
-public class MyConfiguration {
+public class MyConfiguration implements WebMvcConfigurer {
 
-    // Configure InternalResourceViewResolver
+    // Configure TilesConfigurer
     @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions(new String[]{"tilesConfiguration/tiles.xml"});
+        tilesConfigurer.setCheckRefresh(true);
+        return tilesConfigurer;
+    }
+
+    // Configure ViewResolvers to deliver preferred views
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        TilesViewResolver viewResolver = new TilesViewResolver();
+        registry.viewResolver(viewResolver);
     }
 
 }
---
+++ src/main/webapp/menu.jsp
@@ -0,0 +1,2 @@
+<h1>Menu</h1>
+<a href="/appUsers.html">App user page</a>
