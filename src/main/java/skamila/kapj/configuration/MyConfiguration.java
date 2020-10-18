92e6de89a1993f9491d165532601d34304a86a10
Kamila <wiewiora145@gmail.com>
2020-10-18 17:39:53 +0200

    [2] CSS

--- src/main/java/skamila/kapj/configuration/MyConfiguration.java
+++ src/main/java/skamila/kapj/configuration/MyConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
@@ -28,6 +29,13 @@ public class MyConfiguration implements WebMvcConfigurer {
     public void configureViewResolvers(ViewResolverRegistry registry) {
         TilesViewResolver viewResolver = new TilesViewResolver();
         registry.viewResolver(viewResolver);
+    }
+
+
+    //Configure ResourceHandlers to serve static resources like CSS / Javascript etc...
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 
 }
--- src/main/webapp/header.jsp
+++ src/main/webapp/header.jsp
@@ -1,1 +1,6 @@
-<h1>Header</h1>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<link href="<c:url value="/resources/css/header.css" />" rel="stylesheet">
+
+<div class="header">
+    <h1>Header</h1>
+</div>
