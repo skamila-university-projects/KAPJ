1b2daeae7adf0aa1f081fd0340b2e2c383781a8b
Kamila <wiewiora145@gmail.com>
2021-01-01 19:45:55 +0100

    [7] zabezpieczenie przed dostępem do dodawania, edycji i usuwania użytkowników przez nieuprawnione osoby

--- src/main/java/skamila/kapj/configuration/SecurityConfiguration.java
+++ src/main/java/skamila/kapj/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package skamila.kapj.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import javax.annotation.Resource;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Resource(name="myAppUserDetailsService")
--- src/main/java/skamila/kapj/service/AppUserService.java
+++ src/main/java/skamila/kapj/service/AppUserService.java
@@ -1,17 +1,21 @@
 package skamila.kapj.service;
 
+import org.springframework.security.access.annotation.Secured;
 import skamila.kapj.domain.AppUser;
 
 import java.util.List;
 
 public interface AppUserService {
 
+    @Secured("ROLE_ADMIN")
     void addAppUser(AppUser user);
 
+    @Secured("hasRole(ROLE_ADMIN) OR ({#appUser.login == principal.username})")
     void editAppUser(AppUser user);
 
     List<AppUser> listAppUser();
-    
+
+    @Secured("ROLE_ADMIN")
     void removeAppUser(long id);
 
     AppUser getAppUser(long id);
