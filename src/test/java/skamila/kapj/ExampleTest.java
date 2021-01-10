abdc58f5fc5ca47e8a36cdeabcc10e88ff9e9aaf
Kamila <wiewiora145@gmail.com>
2021-01-10 17:19:06 +0100

    [TEST] dodanie przykładowego testu

---
+++ src/test/java/skamila/kapj/ExampleTest.java
@@ -0,0 +1,14 @@
+package skamila.kapj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExampleTest {
+
+    @Test
+    void firstTest() {
+        assertEquals(1, 1);
+    }
+
+}
