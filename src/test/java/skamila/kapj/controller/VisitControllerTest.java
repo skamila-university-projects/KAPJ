b05dc86069ac7c109e3fc625c3975b47962b6493
Kamila <wiewiora145@gmail.com>
2021-01-31 12:41:13 +0100

    [TEST] dodanie testu pobierania listy wizyt dla pacjenta, lekarza i administratora

--- src/test/java/skamila/kapj/controller/VisitControllerTest.java
+++ src/test/java/skamila/kapj/controller/VisitControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.ui.Model;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.support.BindingAwareModelMap;
 import org.springframework.web.servlet.view.RedirectView;
 import skamila.kapj.controller.utils.AppUserBuilder;
 import skamila.kapj.controller.utils.AuthenticationUtils;
@@ -16,6 +17,8 @@ import skamila.kapj.domain.Visit;
 import skamila.kapj.service.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -31,11 +34,11 @@ class VisitControllerTest {
     private final VisitService visitService = new VisitServiceImpl(visitRepositoryMock);
     private final PdfService pdfService = new PdfServiceImpl();
     private final VisitController visitController = new VisitController(appUserService, appUserRoleService, visitService, pdfService);
-    private final AuthenticationUtils authenticationUtils = new AuthenticationUtils();
 
     @Test
     void addVisit() {
         // given
+        AuthenticationUtils authenticationUtils = new AuthenticationUtils();
         authenticationUtils.initSecurityContextMock();
         AppUser currentUser = authenticationUtils.getCurrentUser();
         AppUser selectedDoctor = new AppUserBuilder().withFirstName("Mr.").withLastName("Doctor").build();
@@ -50,6 +53,61 @@ class VisitControllerTest {
         assertEquals(40, visit.getPrice());
         verify(visitRepositoryMock).save(visit);
         assertEquals(redirectView.getUrl(), "/visit/my");
+    }
+
+    @Test
+    void getAllVisits() {
+        // given
+        Model model = new BindingAwareModelMap();
+        List<Visit> visits = new ArrayList<>();
+        visits.add(new Visit());
+        visits.add(new Visit());
+        when(visitRepositoryMock.findAll()).thenReturn(visits);
+        // when
+        String redirect = visitController.getAllVisits(model);
+        // then
+        assertEquals(redirect, "visits");
+        assertEquals(visits, model.asMap().get("visits"));
+        verify(visitRepositoryMock).findAll();
+    }
+
+    @Test
+    void getDoctorVisits() {
+        // given
+        AppUser doctor = new AppUserBuilder().withFirstName("Mr.").withLastName("Doctor").build();
+        AuthenticationUtils authenticationUtils = new AuthenticationUtils(doctor);
+        authenticationUtils.initSecurityContextMock();
+        List<Visit> visits = new ArrayList<>();
+        visits.add(new Visit());
+        visits.add(new Visit());
+        when(appUserRepositoryMock.findByLogin(any())).thenReturn(doctor);
+        Model model = new BindingAwareModelMap();
+        when(visitRepositoryMock.findByDoctor(doctor)).thenReturn(visits);
+        // when
+        String redirect = visitController.getDoctorVisits(model);
+        // then
+        assertEquals(redirect, "visits");
+        assertEquals(visits, model.asMap().get("visits"));
+        verify(visitRepositoryMock).findByDoctor(doctor);
+    }
+
+    @Test
+    void getVisits() {
+        AppUser patient = new AppUserBuilder().withFirstName("Jan").withLastName("Kowalski").build();
+        AuthenticationUtils authenticationUtils = new AuthenticationUtils(patient);
+        authenticationUtils.initSecurityContextMock();
+        List<Visit> visits = new ArrayList<>();
+        visits.add(new Visit());
+        visits.add(new Visit());
+        when(appUserRepositoryMock.findByLogin(any())).thenReturn(patient);
+        Model model = new BindingAwareModelMap();
+        when(visitRepositoryMock.findByPatient(patient)).thenReturn(visits);
+        // when
+        String redirect = visitController.getVisits(model);
+        // then
+        assertEquals(redirect, "visits");
+        assertEquals(visits, model.asMap().get("visits"));
+        verify(visitRepositoryMock).findByPatient(patient);
     }
 
 }
